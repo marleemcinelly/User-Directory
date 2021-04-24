ea186fd873f80065f193351ffa27595109f43dcb
Marlee McInelly <marleemcinelly@gmail.com>
2021-04-23 18:04:48 -0700

    dited key to pull 50 users per request

--- user-directory/src/App.js
+++ user-directory/src/App.js
@@ -29,7 +29,7 @@ function App() {
     })
   }, [])
 
-  console.log(users);
+  console.log('USERS:', users);
 
   return (
     <div className="App">
--- user-directory/src/utils/API.js
+++ user-directory/src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASEURL = 'https://randomuser.me/api/';
+const BASEURL = 'https://randomuser.me/api/?results=50';
 // const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
 
 // Export an object with a "search" method that searches the Giphy API for the passed query
